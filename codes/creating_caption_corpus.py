55eda94291715699bddfc3380003bef8d8a2de92
Hardik Rathod <hardikrathod@Hardik-Rathod-BYOD.local>
2022-02-06 05:05:02 +0000

    modularised the code

--- codes/creating_caption_corpus.py
+++ codes/creating_caption_corpus.py
@@ -7,6 +7,7 @@ import pandas as pd
 import nltk
 import text2emotion as te
 import numpy as np
+import json
 
 def get_emotion_scores(caption_df):
     all_info_dict = {}
@@ -46,42 +47,48 @@ def get_emotion_scores(caption_df):
     #     return(all_info_list)
 
 
-all_themes_captions = pd.read_csv('/Users/hardikrathod/Desktop/captionMe/caption_data/themes_captions.csv')
-# all_captions_1 = all_data_1.iloc[:,2].dropna()
-total_columns = all_themes_captions.shape[0]
+def read_captions_csv(captions_csv_file_path):
+    all_themes_captions = pd.read_csv('/Users/hardikrathod/Desktop/captionMe/caption_data/themes_captions.csv')
+    total_columns = all_themes_captions.shape[0]
 
-beach_captions = all_themes_captions.iloc[:,0].dropna()
-sun_captions = all_themes_captions.iloc[:,1].dropna()
-trees_captions = all_themes_captions.iloc[:,2].dropna()
-car_captions = all_themes_captions.iloc[:,3].dropna()
-people_captions = all_themes_captions.iloc[:,4].dropna()
-dog_captions = all_themes_captions.iloc[:,5].dropna()
-food_captions = all_themes_captions.iloc[:,6].dropna()
-building_captions = all_themes_captions.iloc[:,7].dropna()
-road_captions = all_themes_captions.iloc[:,8].dropna()
+    beach_captions = all_themes_captions.iloc[:,0].dropna()
+    sun_captions = all_themes_captions.iloc[:,1].dropna()
+    trees_captions = all_themes_captions.iloc[:,2].dropna()
+    car_captions = all_themes_captions.iloc[:,3].dropna()
+    people_captions = all_themes_captions.iloc[:,4].dropna()
+    dog_captions = all_themes_captions.iloc[:,5].dropna()
+    food_captions = all_themes_captions.iloc[:,6].dropna()
+    building_captions = all_themes_captions.iloc[:,7].dropna()
+    road_captions = all_themes_captions.iloc[:,8].dropna()
 
-print(len(beach_captions.index))
-print(len(sun_captions.index))
-print(len(trees_captions.index))
-print(len(car_captions.index))
-print(len(people_captions.index))
-print(len(dog_captions.index))
-print(len(food_captions.index))
-print(len(building_captions.index))
-print(len(road_captions.index))
+    print(len(beach_captions.index))
+    print(len(sun_captions.index))
+    print(len(trees_captions.index))
+    print(len(car_captions.index))
+    print(len(people_captions.index))
+    print(len(dog_captions.index))
+    print(len(food_captions.index))
+    print(len(building_captions.index))
+    print(len(road_captions.index))
 
-all_captions_df_dict = {"beach_captions":beach_captions,
-                        "sun_captions":sun_captions,
-                        "trees_captions":trees_captions,
-                        "car_captions":car_captions,
-                        "people_captions":people_captions,
-                        "dog_captions":dog_captions,
-                        "food_captions":food_captions,
-                        "building_captions":building_captions,
-                        "road_captions":road_captions
-                        }
+    all_captions_df_dict = {"beach_captions":beach_captions,
+                            "sun_captions":sun_captions,
+                            "trees_captions":trees_captions,
+                            "car_captions":car_captions,
+                            "people_captions":people_captions,
+                            "dog_captions":dog_captions,
+                            "food_captions":food_captions,
+                            "building_captions":building_captions,
+                            "road_captions":road_captions
+                            }
 
-all_caption_info_dict = {}
-for caption_set in all_captions_df_dict.keys():
-    all_caption_info_dict[caption_set] = get_emotion_scores(all_captions_df_dict[caption_set])
-# print(all_caption_info_dict)
+    all_caption_info_dict = {}
+    for caption_set in all_captions_df_dict.keys():
+        all_caption_info_dict[caption_set] = get_emotion_scores(all_captions_df_dict[caption_set])
+    # print(all_caption_info_dict)
+
+    outfile_id = open("./all_captions_info.json","w")
+    outfile_id.write(json.dumps(all_caption_info_dict))
+    outfile_id.close()
+    print("written all caption data to all_captions_info.json", )
+    # pending writes in Neo4j
